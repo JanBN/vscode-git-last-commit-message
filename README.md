3162b30be1bdbf540bc20b8602e703d5f658e0b2
jpivarcek <jpivarcek@protank.cz>
2019-05-09 15:57:09 +0200

    next iteration

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-# Git load last commit message
+# Git Last Commit Message (GLCM)
 
-Load your last commit message into git commit box.
-## Press CTRL+K CTRL+L (can be changed in keybindings)
-OR
-## Choose from menu 
+Load your last commit message into git commit box, or let you choose from last n messages.
+
+## Screenshots
+
 ![](https://raw.githubusercontent.com/JanBN/vscode-git-last-commit-message/master/assets/menu.PNG)
-
-OR
-## Run as a command
 ![](https://raw.githubusercontent.com/JanBN/vscode-git-last-commit-message/master/assets/command.PNG)
 
+
+## Commands
+
+**(GLCM) Load last commit message** - Loads last commit message into commit message box
+
+**(GLCM) Choose commit message from last messages** - Let you choose from your last commit message and sets it as commit message
+
+
+## Shortcuts
+
+**CTRL+L CTRL+L** - Loads last commit message into commit message box
+
+**CTRL+L CTRL+K** - Let you choose from your last commit message and sets it as commit message
+
+## Extension Settings
+
+```
+"glcm.countOfCommitMessagesToChooseFrom": 10
+```
 
 <div>Icons made by <a href="https://www.freepik.com/?__hstc=57440181.75a3ff57afa70083ceeb2f78b1513303.1557398427456.1557398427456.1557398427456.1&__hssc=57440181.2.1557398427456&__hsfp=3840035087" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- src/LastCommitMessage.ts
+++ src/LastCommitMessage.ts
@@ -4,10 +4,16 @@ import * as Git from './@types/git';
 
 export class LastCommitMessage {
     _gitAPI: Git.API;
+    _config = vscode.workspace.getConfiguration('glcm');
 
     constructor(context: vscode.ExtensionContext) {
         this._gitAPI = vscode.extensions.getExtension('vscode.git').exports.getAPI(1);
-        vscode.commands.registerCommand('extension.loadLastCommitMessage', () => this.loadLastCommitMessage());
+        vscode.commands.registerCommand('glcm.loadLastCommitMessage', () => this.loadLastCommitMessage());
+        vscode.commands.registerCommand('glcm.chooseLastCommitMessage', () => this.chooseLastCommitMessage());
+
+        vscode.workspace.onDidChangeConfiguration(() => {
+            this._config = vscode.workspace.getConfiguration('glcm');
+        })
     }
 
     async loadLastCommitMessage() {
@@ -21,6 +27,50 @@ export class LastCommitMessage {
             } catch (ex) {
             }
         });
+    }
+
+    async chooseLastCommitMessage() {
+        const selectedRep = this._gitAPI.repositories.find(x => x.ui.selected);
+        if (selectedRep != null) {
+
+
+            const countOfCommitMessagesToChooseFrom = this._config.countOfCommitMessagesToChooseFrom;
+            const commitMessages = [];
+            await this.getParentCommitRecursivly(0, countOfCommitMessagesToChooseFrom, selectedRep, [selectedRep.state.HEAD.commit], commitMessages);
+
+            vscode.window.showQuickPick(commitMessages, {
+                canPickMany: false,
+                placeHolder: "Choose commit message"
+            }).then(item => {
+                if (item) {
+                    selectedRep.inputBox.value = item;
+                }
+            });
+        }
+    }
+
+    async getParentCommitRecursivly(i: number, limit: number, rep: Git.Repository, commitHashes: string[], messages: string[]): Promise<number> {
+        if (!commitHashes) {
+            return i;
+        }
+
+        if (!commitHashes || commitHashes.length == 0) {
+            return i;
+        }
+
+        for (let index = 0; index < commitHashes.length; index++) {
+            if (i >= limit) break;
+            const parentCommitHash = commitHashes[index];
+            const parentCommit = await rep.getCommit(parentCommitHash);
+            if (parentCommit) {
+                i++;
+                messages.push(parentCommit.message);
+                const ranCount = await this.getParentCommitRecursivly(i, limit, rep, parentCommit.parents, messages);
+                i = i + ranCount;
+            }
+        }
+
+        return i;
     }
 
     dispose() {
